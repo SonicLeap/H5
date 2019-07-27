009fa00d17e6e59928c73ab4db17aee7f01a6cab
SonicLeap <49132520+SonicLeap@users.noreply.github.com>
2019-07-27 14:49:15 +0800

    Add files via upload

---
+++ p5demo01/sketch.js
@@ -0,0 +1,22 @@
+//
+
+var pic;
+function preload(){
+	pic =loadImage("images/huaji.png")
+}
+function setup() {
+	createCanvas(1080,1920);
+	
+}
+var posX=0;//位置
+var posY=0;
+var speedX=5;//速度
+var speedY=5;
+function draw() {
+	background(30);//背景颜色
+	posX=posX+speedX;//新位置
+	image(pic,posX,height/2-40,80,80);
+	if(posX==width-80||posX==0){
+		speedX*=-1;
+	}
+}
